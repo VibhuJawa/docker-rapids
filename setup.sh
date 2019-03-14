5c7f117b9f476225bf8734e61e956741bdcdc4fd
Vibhu Jawa <vibhujawa@gmail.com>
2019-03-13 17:05:00 -0700

    commented_ml_parts

--- setup.sh
+++ setup.sh
@@ -5,17 +5,17 @@ git clone --recurse-submodules https://github.com/rapidsai/custrings
 # cloning directly from strings WIP pr
 git clone --recurse-submodules https://github.com/kkraus14/cudf -b fea-ext-string-support
 
-git clone --recurse-submodules https://github.com/rapidsai/cuml -b branch-0.6
+# git clone --recurse-submodules https://github.com/rapidsai/cuml -b branch-0.6
 
 git clone --recurse-submodules https://github.com/rapidsai/cugraph -b branch-0.6
 
 git clone --recurse-submodules https://github.com/rapidsai/dask-cuda
 git clone --recurse-submodules https://github.com/rapidsai/dask-cudf
 
-git clone --recurse-submodules https://github.com/rapidsai/dask-xgboost
+# git clone --recurse-submodules https://github.com/rapidsai/dask-xgboost
 
-git clone --recursive https://github.com/rapidsai/xgboost -b cudf-interop rapidsai-xgboost
-cd rapidsai-xgboost && git submodule update --init --recursive -- dmlc-core
+# git clone --recursive https://github.com/rapidsai/xgboost -b cudf-interop rapidsai-xgboost
+# cd rapidsai-xgboost && git submodule update --init --recursive -- dmlc-core
 
 #git clone --recursive https://github.com/dmlc/xgboost.git
 #git clone --recurse-submodules https://github.com/dmlc/xgboost
