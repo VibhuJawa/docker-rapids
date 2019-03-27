92a8b6aa0438ce1eadf62b36418518634ba853ef
Vibhu Jawa <vibhujawa@gmail.com>
2019-03-27 09:36:11 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ docker run --runtime=nvidia --rm -it -p 9888:9888 -p 9787:9787 -p 9786:9786  -p 
 -v /raid/walmart/vibhu/wallmart_data:/rapids/wallmart_data \
 -v /raid/walmart/vibhu/walmart_sparksql_joins:/rapids/walmart_sparksql_joins \
 -e NVIDIA_VISIBLE_DEVICES=1,2,3,4,5,6,7 \
--it rapids-strings
+-it rapids
